4fe162827d8619e47ec463a0fcc4db68fc222143
Anandaalf <115127421+Anandaalf@users.noreply.github.com>
2022-11-24 11:06:33 +0700

    Create variabel.java

---
+++ variabel.java
@@ -0,0 +1,11 @@
+class Main 
+{
+      public static void main(String[] args) {
+      String nama = "Ananda Alfiah";
+      int umur = 2004;
+
+      System.out.println("Assalamu'alaikum");
+      System.out.println("Nama saya : "+ nama);
+      System.out.println("Saya lahir ditahun : "+ umur);
+    }
+}
